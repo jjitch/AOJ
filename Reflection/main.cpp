69c1c29c5b09da77e3f32b04f7cf5f47673174e4
Junnosuke Yoshida <junnosuke_08@outlook.jp>
2021-03-02 19:17:31 +0900

    start reflection

---
+++ Reflection/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+template<typename T>
+class Vec2
+{
+
+public:
+	T x;
+	T y;
+	Vec2 operator - (Vec2 obj) {
+		return Vec2(this->x - obj.x, this->y - obj.y);
+	}
+	Vec2 operator + (Vec2 obj) {
+		return Vec2(this->x + obj.x, this->y + obj.y);
+	}
+	T operator * (Vec2 obj) {
+		return this->x * obj.x + this->y * obj.y;
+	}
+	template<class U>
+	Vec2 operator * (U t) {
+		return Vec2(this->x * t, this->y * t);
+	}
+	Vec2(T x, T y) {
+		this->x = x;
+		this->y = y;
+	}
+};
+
+int main() {
+	double a = 0.7;
+	std::cout << std::setprecision(2) << a << std::endl;
+}
