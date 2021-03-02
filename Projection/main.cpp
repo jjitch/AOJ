725c44e0c84e4b076b5d11838477c4b0a20bf3ad
jjitch <junnosuke_08@icloud.com>
2021-03-02 13:12:43 +0900

    complete Vec2

--- Projection/main.cpp
+++ Projection/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 template<typename T>
 class Vec2
@@ -10,8 +12,15 @@ public:
 	Vec2 operator - (Vec2 obj) {
 		return Vec2(this->x - obj.x, this->y - obj.y);
 	}
+	Vec2 operator + (Vec2 obj) {
+		return Vec2(this->x + obj.x, this->y + obj.y);
+	}
 	T operator * (Vec2 obj) {
 		return this->x * obj.x + this->y * obj.y;
+	}
+	template<class U>
+	Vec2 operator * (U t) {
+		return Vec2(this->x * t, this->y * t);
 	}
 	Vec2(T x, T y) {
 		this->x = x;
@@ -20,7 +29,21 @@ public:
 };
 
 int main() {
-	Vec2<int> a(5, 6);
-	Vec2<int> b(3, 7);
-	std::cout << a * b << std::endl;
+	double x1, y1, x2, y2;
+	std::cin >> x1 >> y1 >> x2 >> y2;
+	Vec2<double> p1(x1, y1);
+	Vec2<double> p2 = Vec2<double>(x2, y2) - p1;
+	int q;
+	std::cin >> q;
+	std::vector<Vec2<double>> ans(q,Vec2<double>(0,0));
+	for (int i = 0; i < q; i++)
+	{
+		double x, y;
+		std::cin >> x >> y;
+		Vec2<double> p = Vec2<double>(x, y) - p1;
+		ans[i] = p1 + p2 * ((p * p2) / (p2 * p2));
+	}
+	for (int i = 0; i < q; i++) {
+		std::cout <<std::setprecision(18)<< ans[i].x << " " << ans[i].y << std::endl;
+	}
 }
