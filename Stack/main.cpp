a6404c4159056cec23f2bbb2bc4f629ded193276
Junnosuke Yoshida <junnosuke_08@outlook.jp>
2021-03-10 18:57:49 +0900

    implement stack

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
 #include <stack>
 using namespace std;
 
 int main() {
+
+    stack<int> S;
+    int a, b;
+    string t;
+    while (cin >> t) {
+        if (t[0] == '+') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            S.push(a + b);
+        }
+        else if (t[0] == '-') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            S.push(b - a);
+        }
+        else if (t[0] == '*') {
+            a = S.top(); S.pop();
+            b = S.top(); S.pop();
+            S.push(a * b);
+        }
+        else {
+            S.push(atoi(t.c_str()));
+        }
+    }
+    cout << S.top() << endl;
 }
+
