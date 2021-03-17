e7448aaf9d85f8506c7a08ed5f89cc9ec8de609b
Junnosuke Yoshida <junnosuke_08@outlook.jp>
2021-03-17 14:37:01 +0900

    slight format change

--- SingleSourceShortestPath/main.cpp
+++ SingleSourceShortestPath/main.cpp
@@ -10,12 +10,13 @@ using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
 
-const ll INF = 1LL<<60;
+const ll INF = 1LL << 60;
 
 vector<ll> dis;
 vector<vector<P>> G;
 
-void dijkstra(size_t start) {
+void dijkstra(size_t start)
+{
 	priority_queue<P, vector<P>, greater<P>> pq;
 	P init(0, start);
 	dis[start] = 0;
@@ -25,9 +26,10 @@ void dijkstra(size_t start) {
 		P seeking = pq.top();
 		pq.pop();
 		if (seeking.cost > dis[seeking.vertex]) continue;
-		for (P next:G[seeking.vertex])
+		for (P next : G[seeking.vertex])
 		{
-			if (dis[next.vertex] > dis[seeking.vertex] + next.cost) {
+			if (dis[next.vertex] > dis[seeking.vertex] + next.cost)
+			{
 				dis[next.vertex] = dis[seeking.vertex] + next.cost;
 				pq.emplace(dis[next.vertex], next.vertex);
 			}
@@ -35,20 +37,25 @@ void dijkstra(size_t start) {
 	}
 }
 
-int main() {
+
+int main()
+{
 	size_t v, e, s;
 	cin >> v >> e >> s;
 	G.resize(v);
-	dis.resize(v,INF);
-	rep(i, e) {
+	dis.resize(v, INF);
+	rep(i, e)
+	{
 		ll v;
 		P p;
 		cin >> v >> p.vertex >> p.cost;
 		G[v].push_back(p);
 	}
 	dijkstra(s);
-	rep(i, v) {
-		if (dis[i] == INF) {
+	rep(i, v)
+	{
+		if (dis[i] == INF)
+		{
 			cout << "INF" << endl;
 			continue;
 		}
