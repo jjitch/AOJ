1269197a74365fc2b41025eccbaf8254d7b7529e
Junnosuke Yoshida <junnosuke_08@outlook.jp>
2021-03-21 15:08:13 +0900

    format!!!

--- EulersPhiFunction/main.cpp
+++ EulersPhiFunction/main.cpp
@@ -7,7 +7,7 @@ using ll = unsigned long long;
 
 ll EPF(ll n)
 {
-	ll ret=n;
+	ll ret = n;
 	unordered_set<ll> s;
 	for (size_t i = 2; i <= (size_t)sqrt(n); i++)
 	{
@@ -17,7 +17,7 @@ ll EPF(ll n)
 			n /= i;
 		}
 	}
-	if (n!=1) s.insert(n);
+	if (n != 1) s.insert(n);
 	for (auto v : s)
 	{
 		ret *= (v - 1);
