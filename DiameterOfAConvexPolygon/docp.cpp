2de48f3cc62244c9f41d75b3e23c433f6c0e440e
jjitch <junnosuke_08@icloud.com>
2021-04-10 23:45:35 +0900

    create all projects

---
+++ DiameterOfAConvexPolygon/docp.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <stdlib.h>
+#include <math.h>
+#include <algorithm>
+
+class Vec2
+{
+	double x;
+	double y;
+public:
+	Vec2() :x(0), y(0) {};
+	Vec2(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	const double getX() const { return x; }
+	const double getY() const { return y; }
+	const double getAngle() const { return atan(y / x); }
+	void rotate(const double& theta)
+	{
+		double tmpX = x * cos(theta) - y * sin(theta);
+		double tmpY = x * sin(theta) + y * cos(theta);
+		x = tmpX;
+		y = tmpY;
+	}
+	void flipX() { x *= -1; }
+	void flipY() { y *= -1; }
+
+	// Assignment operator
+
+	Vec2& operator = (const Vec2& other)
+	{
+		x = other.x;
+		y = other.y;
+		return *this;
+	}
+
+	// Binary operator
+
+	const Vec2 operator + (const Vec2& other) const
+	{
+		return Vec2(x + other.x, y + other.y);
+	}
+	const Vec2 operator - (const Vec2& other) const
+	{
+		return Vec2(x - other.x, y - other.y);
+	}
+	// Inner product
+	const double operator * (const Vec2& other) const
+	{
+		return x * other.x + y * other.y;
+	}
+	template<class U> const Vec2 operator * (const U& t) const
+	{
+		return Vec2(x * t, y * t);
+	}
+	template<class U> friend const Vec2 operator * (const U& a, const Vec2& vec2)
+	{
+		return Vec2(vec2.x * a, vec2.y * a);
+	}
+	// Outer product
+	const double operator % (const Vec2& other) const
+	{
+		return x * other.y - y * other.x;
+	}
+
+	//Urary operator
+
+	Vec2& operator+=(const Vec2& other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+	Vec2& operator-=(const Vec2& other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	// IO operator
+
+	friend std::ostream& operator << (std::ostream& os, const Vec2& vec2)
+	{
+		std::cout << vec2.x << " " << vec2.y;
+		return os;
+	}
+	friend std::istream& operator >> (std::istream& is, Vec2& vec2)
+	{
+		std::cin >> vec2.x >> vec2.y;
+		return is;
+	}
+
+	// Relatinal operator
+
+	bool operator < (const Vec2& other) const { return x < other.x || (!(other.x < x) && y < other.y); }
+	bool operator > (const Vec2& other) const { return x > other.x || (!(other.x > x) && y > other.y); }
+	bool operator <= (const Vec2& other) const { return !(*this > other); }
+	bool operator >= (const Vec2& other) const { return !(*this < other); }
+	bool operator == (const Vec2& other) const { return x == other.x && y == other.y; }
+	bool operator != (const Vec2& other) const { return !(*this == other); }
+
+};
+
+int main()
+{
+	using namespace std;
+	size_t n;
+	cin >> n;
+	vector<Vec2> polygon(n);
+	for (size_t i = 0; i < n; i++) cin >> polygon[i];
+
+
+}
