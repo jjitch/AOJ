dba11dc8b3e38a686b0141846a3a9b01309005ef
Junnosuke Yoshida <junnosuke_08@outlook.jp>
2021-04-22 14:52:43 +0900

    regardless num of intersection

--- ManhattanReturns/manhattanReturns.cpp
+++ ManhattanReturns/manhattanReturns.cpp
@@ -9,8 +9,7 @@
 #include <map>
 #include <cassert>
 
-
-std::map<int, size_t> m;
+std::map<int, size_t> compressor;
 
 enum class OPERATION
 {
@@ -75,46 +74,23 @@ public:
 		nodes[0]--;
 	}
 	T getSum(size_t queryLeft, size_t queryRight) { return subQuery(queryLeft, queryRight, 0, 0, numLeaf); }
-
-	friend std::ostream& operator<<(std::ostream& os, const SegTree& st)
-	{
-		std::cout << "------------\n";
-		std::cout << "number of leaves: " << st.numLeaf << "\n";
-		std::size_t splitFlag = 0;
-		for (std::size_t i = 0; i < st.nodes.size(); i++)
-		{
-			std::cout << st.nodes[i] << " ";
-			if (i == splitFlag)
-			{
-				std::cout << "\n";
-				splitFlag = 2 * (splitFlag + 1);
-			}
-		}
-		std::cout << "------------\n";
-		return os;
-	}
 };
 
-class eventQueue
+class eventOperator
 {
 	int order;
 	OPERATION ope;
 	int x1, y1, x2, y2;
 public:
-	eventQueue(int _order, OPERATION _ope, int _x1, int _y1, int _x2, int _y2) :
-		order(_order), ope(_ope), x1(_x1), y1(_y1), x2(_x2), y2(_y2) {}
-	bool operator < (eventQueue rhs)
+	eventOperator(int _order, OPERATION _ope, int _x1, int _y1, int _x2, int _y2) :
+		order(_order), ope(_ope)
 	{
-		if (order != rhs.order)
-		{
-			return order < rhs.order;
-		}
-		else
-		{
-			return (int)ope < (int)rhs.ope;
-		}
+		x1 = std::min(_x1, _x2);
+		y1 = std::min(_y1, _y2);
+		x2 = std::max(_x1, _x2);
+		y2 = std::max(_y1, _y2);
 	}
-	bool operator > (eventQueue rhs)
+	bool operator > (const eventOperator& rhs) const 
 	{
 		if (order != rhs.order)
 		{
@@ -131,67 +107,55 @@ public:
 		std::size_t res = 0;
 		if (ope == OPERATION::INSERT)
 		{
-			st.plusOne(m[y1]);
+			st.plusOne(compressor[y1]);
 		}
 		else if (ope == OPERATION::ERASE)
 		{
-			st.minusOne(m[y1]);
+			st.minusOne(compressor[y1]);
 		}
 		else
 		{
-			res = st.getSum(m[y1], m[y2]);
+			res = st.getSum(compressor[y1], compressor[y2]+1);
 		}
 		return res;
 	}
 };
 
-void testSegTree()
-{
-	using namespace std;
-	SegTree<size_t> st(4);
-	st.plusOne(0);
-	st.plusOne(1);
-	st.plusOne(5);
-	cout << st << endl;
-	//assert(st.getSum(0, 7) == 2);
-}
-
 int main()
 {
 	using namespace std;
-	auto c = [](eventQueue l, eventQueue r) {return l > r; };
 	size_t n;
 	cin >> n;
-	priority_queue<eventQueue, vector<eventQueue>, decltype(c)> pq(c);
-	set<int> s;
+	priority_queue<eventOperator, vector<eventOperator>, greater<eventOperator>> eventQueue;
+	set<int> noDuplicatedY;
 	int x1, y1, x2, y2;
-	for (size_t i = 0; i < n; i++)
+	while(n--)
 	{
 		cin >> x1 >> y1 >> x2 >> y2;
 		if (x1 == x2)
 		{
-			pq.emplace(x1, OPERATION::SEARCH, x1, min(y1, y2), x2, max(y1, y2));
+			eventQueue.emplace(x1, OPERATION::SEARCH, x1, y1, x2, y2);
 		}
 		else
 		{
-			pq.emplace(min(x1, x2), OPERATION::INSERT, min(x1, x2), y1, max(x1, x2), y2);
-			pq.emplace(max(x1, x2), OPERATION::ERASE, min(x1, x2), y1, max(x1, x2), y2);
+			eventQueue.emplace(min(x1, x2), OPERATION::INSERT, x1, y1, x2, y2);
+			eventQueue.emplace(max(x1, x2), OPERATION::ERASE, x1, y1, x2, y2);
 		}
-		s.insert(y1);
-		s.insert(y2);
+		noDuplicatedY.insert(y1);
+		noDuplicatedY.insert(y2);
 	}
 	size_t compressedIndex = 0;
-	for (auto y = s.begin(); y!=s.end();y++)
+	for (auto y = noDuplicatedY.begin(); y!=noDuplicatedY.end();y++)
 	{
-		m.emplace(*y, compressedIndex);
+		compressor.emplace(*y, compressedIndex);
 		compressedIndex++;
 	}
 	SegTree<size_t> horizons(compressedIndex);
 	size_t ans = 0;
-	while (!pq.empty())
+	while (!eventQueue.empty())
 	{
-		eventQueue q = pq.top();
-		pq.pop();
+		eventOperator q = eventQueue.top();
+		eventQueue.pop();
 		ans += q.execute(horizons);
 	}
 	cout << ans << endl;
